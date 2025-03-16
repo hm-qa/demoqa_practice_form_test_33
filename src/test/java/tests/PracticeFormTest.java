9850fc8713cb9e250dabc0a8a7fbcb741381907d
hm-qa <rtk.it.hm@gmail.com>
2025-03-17 00:08:39 +0300

    started working with PracticeFormTest

---
+++ src/test/java/tests/PracticeFormTest.java
@@ -0,0 +1,43 @@
+package tests;
+
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import java.beans.PropertyEditor;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+
+public class PracticeFormTest {
+
+    @BeforeAll
+    static void beforeAll() {
+        Configuration.browserSize = "1920x1080";
+        Configuration.baseUrl = "https://demoqa.com";
+        Configuration.pageLoadStrategy = "eager";
+        Configuration.holdBrowserOpen = true;
+        Configuration.timeout = 5000; // default 4000
+    }
+
+    @Test
+    void fillFormTest() {
+        open("/automation-practice-form");
+        $("#firstName").setValue("Alex");
+        $("#lastName").setValue("Tester");
+        $("#userEmail").setValue("alex@tester.com");
+        //gender
+        $("#userNumber").setValue("9876543210");
+        $("#dateOfBirthInput").setValue("9876543210");
+        $("#currentAddress").setValue("Another street 1");
+        $("#uploadPicture").setValue("Another street 1");
+        $("#submit").click();
+
+/*        $("#output #name").shouldHave(text("Alex"));
+        $("#output #email").shouldHave(text("alex@egorov.com"));
+        $("#output #currentAddress").shouldHave(text("Some street 1"));
+        $("#output #permanentAddress").shouldHave(text("Another street 1"));*/
+    }
+
+}
